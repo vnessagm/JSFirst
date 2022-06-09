0e2bf01554479fc9d86700445f5d968f33d7d77f
Vanessa Gonçalves <98989640+vnessagm@users.noreply.github.com>
2022-06-09 12:16:55 -0300

    Simple script

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "Vanessa"
+console.log(name)
+let number = 20
+let year = 2022
+let sub = year - number
+console.log(sub)
+let sum = year + number
+console.log(sum)
+let text = "Estou mexendo no console.log"
+console.log(text)
+let quartaFeira = false
+console.log(typeof quartaFeira)
